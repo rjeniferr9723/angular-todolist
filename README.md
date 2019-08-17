b1b29ed5315ea09e94587097483ce2d148eee043
rjeniferr9723 <rjeniferr9723@gmail.com>
2019-08-17 18:46:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5ibdnu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5ibdnu)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  tasks = [];
+  addTask(task){
+    this.tasks.push(task)
+  }
+
+}
